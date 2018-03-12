b7b658432d1b80374bffbf8aba71951bccfb7efd
Kentaro Sasaki <accelerk@gmail.com>
2018-03-13 02:23:47 +0900

    security-best-practiceを適用

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+// For Security, Disable X-Powered-By header
+app.disable('x-powered-by');
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
